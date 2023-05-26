89063d3810e61b707250f162856fdea876f1233e
jimano1 <jimano.ads@gmail.com>
2023-05-26 00:13:02 +0000

    passed

--- simple_shell.c
+++ simple_shell.c
@@ -1,8 +1,5 @@
 #include "shell.h"
 
-/* global variable for ^C handling */
-unsigned int sig_flag;
-
 /**
  * sig_handler - handles ^C signal interupt
  * @uuv: unused variable (required for signal function prototype)
